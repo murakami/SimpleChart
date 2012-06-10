0e2216d102826cb640bff0848e8b23fba2d18251
murakami <m_yukio@mac.com>
2012-06-10 22:33:53 +0900

    SimpleChart

---
+++ SimpleChart/SimpleChartView.h
@@ -0,0 +1,13 @@
+//
+//  SimpleChartView.h
+//  SimpleChart
+//
+//  Created by 村上 幸雄 on 12/06/10.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SimpleChartView : UIView
+
+@end
---
+++ SimpleChart/SimpleChartView.m
@@ -0,0 +1,35 @@
+//
+//  SimpleChartView.m
+//  SimpleChart
+//
+//  Created by 村上 幸雄 on 12/06/10.
+//  Copyright (c) 2012年 ビッツ有限会社. All rights reserved.
+//
+
+#import "SimpleChartView.h"
+
+@interface SimpleChartView ()
+
+@end
+
+@implementation SimpleChartView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
