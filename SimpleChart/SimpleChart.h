5624b44c87a76d290cc680f451a3bbbc9a33886a
Yukio MURAKAMI <m_yukio@mac.com>
2013-11-23 19:58:55 +0900

    SimpleChart.framework

---
+++ SimpleChart/SimpleChart.h
@@ -0,0 +1,13 @@
+//
+//  SimpleChart.h
+//  SimpleChart
+//
+//  Created by 村上幸雄 on 2013/11/23.
+//  Copyright (c) 2013年 村上幸雄. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleChart : NSObject
+
+@end
---
+++ SimpleChart/SimpleChart.m
@@ -0,0 +1,13 @@
+//
+//  SimpleChart.m
+//  SimpleChart
+//
+//  Created by 村上幸雄 on 2013/11/23.
+//  Copyright (c) 2013年 村上幸雄. All rights reserved.
+//
+
+#import "SimpleChart.h"
+
+@implementation SimpleChart
+
+@end
